36c39c3e0cbf568ed96625173cb4032cb5c1aa61
xGeekoo <64699089+xGeekoo@users.noreply.github.com>
2024-05-13 18:56:38 -0400

    add bottom padding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import CodeEditor from './components/CodeEditor';
 
 function App() {
   return (
-    <div className="mx-auto max-w-[56.25rem] px-4 pt-9">
+    <div className="mx-auto max-w-[56.25rem] px-4 py-9">
       <header className="mb-9">
         <h2 className="mb-9">
           <img
